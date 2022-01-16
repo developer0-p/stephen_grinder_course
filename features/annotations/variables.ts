e28c7c75ee2ad3362d0a48b877008646c6d8137d
Pablo <pablo.developer0@gmail.com>
2022-01-16 15:49:35 +0100

    typescript 583

---
+++ features/annotations/variables.ts
@@ -0,0 +1,17 @@
+let apples: number = 5;
+let speed: string = 'fast'
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+let now: Date = new Date();
+let colors: string[] = ['green','red','blue']
+let myNumbers: number[] = [1,2,3];
+
+class Car {
+
+}
+let car: Car = new Car()
+
